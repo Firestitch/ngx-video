76675ad041df8a3830438b868e7ca85762b52d9b
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-03-28 10:52:24 +0300

    removed wrong tools

--- tools/components/examples/examples.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'fs-examples',
-  templateUrl: 'examples.component.html'
-})
-export class FsExamplesComponent {
-  @Input() public title: string;
-}
