5a4ca730f162ed60c88bad96245d2362b603ce70
Natalia Zhornitskaya <n.zhornitskaya@wis.eu.com>
2018-09-21 15:03:02 +0300

    GO-T1927 (fix): changed url for example without skipping ad

--- playground/app/components/video-pre-roll-ad-example/video-pre-roll-ad-example.component.ts
+++ playground/app/components/video-pre-roll-ad-example/video-pre-roll-ad-example.component.ts
@@ -24,7 +24,7 @@ export class VideoPreRollAdExampleComponent implements OnInit {
       ads: [
         {
           type: 'pre',
-          source: 'http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8',
+          source: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
           url: 'http://shopforshoes.com',
           label: 'Visit shopforshoes.com',
         }
